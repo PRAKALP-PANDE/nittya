ba8520c7aa221e7d436a1ee113bc248961231291
PRAKALP-PANDE <prakalpsp16@gmail.com>
2025-04-11 00:52:02 +0530

    RT: Carousel button

--- Components/HappyClients.js
+++ Components/HappyClients.js
@@ -3,8 +3,22 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import CardHappyClient from './CardHappyClient';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const HappyClients = () => {
+
+    const CustomPrevArrow = ({ onClick }) => (
+        <button onClick={onClick} className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 bg-transparent p-3 transition-all">
+            <FaChevronLeft size={35} />
+        </button>
+    );
+
+    const CustomNextArrow = ({ onClick }) => (
+        <button onClick={onClick} className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 bg-transparent p-3 transition-all">
+            <FaChevronRight size={35} />
+        </button>
+    );
+
     const settings = {
         dots: true,
         infinite: true,
@@ -13,12 +27,14 @@ const HappyClients = () => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000, // Adjust as needed
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
     };
 
     return (
         <div id='happyCustomers' className="relative p-10 md:py-12 bg-gray-100">
-            <div className="container mx-auto text-center md:py-12">
-                <h2 className="text-3xl font-bold mb-4">Happy Clients</h2>
+            <div className="container mx-auto text-center">
+                <h2 className="max-w-2xl mb-4 md:mb-10 text-4xl sm:text-5xl md:text-8xl cookie-regular font-semibold tracking-tight leading-tight text-[#2d9155] inline-flex items-center drop-shadow-md">Happy Clients</h2>
                 <p className="text-gray-600">Discover a wide range of nutrition products to support your wellness journey.</p>
             </div>
             <Slider {...settings}>
--- Components/HealthConditionDiet.js
+++ Components/HealthConditionDiet.js
@@ -6,9 +6,24 @@ import 'slick-carousel/slick/slick-theme.css';
 import BloodPressure from './DietPlans/BloodPressure';
 import FattyLiver from './DietPlans/FattyLiver';
 import Thyroid from './DietPlans/Thyroid';
-import PCOS from './DietPlans/PCOS';
+import PCOS from './DietPlans/Pcos';
+import PregnancyDIetPlan from './DietPlans/PregnancyDIetPlan';
+import PostNatal from './DietPlans/PostNatal';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const HealthConditionDiet = () => {
+
+    const CustomPrevArrow = ({ onClick }) => (
+        <button onClick={onClick} className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 bg-transparent p-3 transition-all">
+            <FaChevronLeft size={35} />
+        </button>
+    );
+
+    const CustomNextArrow = ({ onClick }) => (
+        <button onClick={onClick} className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 bg-transparent p-3 transition-all">
+            <FaChevronRight size={35} />
+        </button>
+    );
     const settings = {
         dots: true,
         infinite: true,
@@ -16,6 +31,8 @@ const HealthConditionDiet = () => {
         speed: 500,
         slidesToShow: 1, // Number of slides to show at a time
         slidesToScroll: 1, // Number of slides to scroll on navigation
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
         responsive: [
             {
                 breakpoint: 1024,
@@ -33,18 +50,20 @@ const HealthConditionDiet = () => {
     };
 
     return (
-        <section className="py-10 md:py-20 bg-gray-200">
+        <section className="py-5 md:py-10 bg-gray-100">
             <div className="container px-5 mx-auto flex flex-wrap">
                 <h2 className="mb-10 md:mb-16 text-4xl sm:text-5xl md:text-6xl cookie-regular font-normal leading-16 text-[#2f9457] text-center w-full">
                     Diet Plans According To Health Conditions
                 </h2>
-                <div className="w-full m-5 px-5">
+                <div className="w-full px-5">
                     <Slider {...settings}>
                         <Diabetes />
                         <BloodPressure />
                         <FattyLiver />
                         <Thyroid />
                         <PCOS />
+                        <PregnancyDIetPlan />
+                        <PostNatal />
                     </Slider>
                 </div>
             </div>
