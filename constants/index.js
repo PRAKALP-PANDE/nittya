40d381fde995bb15289fedb16f7be6d9827063a8
PRAKALP-PANDE <prakalpsp16@gmail.com>
2025-05-22 23:59:11 +0530

    FIX: removed unused objects

--- constants/index.js
+++ constants/index.js
@@ -1,36 +1,3 @@
-// const productsList = [
-//     {
-//         title: "This is title",
-//         image: "https://www.mightymillets.com/upload/product/953C8A66-C507-4860-A5B0-342E23E13AB2.jpg",
-//         description: "100% Pure Canadian Maple syrup as a sweetener Totally pure, natural and healthier alternative to heated honey or refined sugar. It is rich in antioxidants, is diabetic friendly and rich in nutrients. More nuts, less oats Significantly higher ratio of nuts, seeds and dry",
-//         price: 100
-//     },
-//     {
-//         title: "This is title",
-//         image: "https://www.mightymillets.com/upload/product/953C8A66-C507-4860-A5B0-342E23E13AB2.jpg",
-//         description: "100% Pure Canadian Maple syrup as a sweetener Totally pure, natural and healthier alternative to heated honey or refined sugar. It is rich in antioxidants, is diabetic friendly and rich in nutrients. More nuts, less oats Significantly higher ratio of nuts, seeds and dry",
-//         price: 100
-//     },
-//     {
-//         title: "This is title",
-//         image: "https://www.mightymillets.com/upload/product/953C8A66-C507-4860-A5B0-342E23E13AB2.jpg",
-//         description: "100% Pure Canadian Maple syrup as a sweetener Totally pure, natural and healthier alternative to heated honey or refined sugar. It is rich in antioxidants, is diabetic friendly and rich in nutrients. More nuts, less oats Significantly higher ratio of nuts, seeds and dry",
-//         price: 100
-//     },
-//     {
-//         title: "This is title",
-//         image: "https://www.mightymillets.com/upload/product/953C8A66-C507-4860-A5B0-342E23E13AB2.jpg",
-//         description: "100% Pure Canadian Maple syrup as a sweetener Totally pure, natural and healthier alternative to heated honey or refined sugar. It is rich in antioxidants, is diabetic friendly and rich in nutrients. More nuts, less oats Significantly higher ratio of nuts, seeds and dry",
-//         price: 100
-//     },
-//     {
-//         title: "This is title",
-//         image: "https://www.mightymillets.com/upload/product/953C8A66-C507-4860-A5B0-342E23E13AB2.jpg",
-//         description: "100% Pure Canadian Maple syrup as a sweetener Totally pure, natural and healthier alternative to heated honey or refined sugar. It is rich in antioxidants, is diabetic friendly and rich in nutrients. More nuts, less oats Significantly higher ratio of nuts, seeds and dry",
-//         price: 100
-//     }
-// ];
-
 const nutritionPlans = [
     {
         image: "icon_writing_pad.png",
