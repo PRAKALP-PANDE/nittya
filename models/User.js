3c0d35db3045463e7ef4f1be8ab01515cf4ce2b8
PRAKALP-PANDE <prakalpsp16@gmail.com>
2025-05-25 23:22:09 +0530

    ENH: Added new column for superAdmin

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false }, 
+    isSuperAdmin: { type: Boolean, default: false }, 
     
 }, { timestamps: true });
 // mongoose.models = {}
